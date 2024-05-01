0c16437bfb98edc2cc1a7e8425b929afc3810185
Amit Kumar Saini <amitsaini8385@gmail.com>
2024-05-01 20:43:17 +0530

    create a fibonacci series

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the number");
+    let mut num: String = String::new();
+
+    io::stdin().read_line(&mut num).expect("Failed to read");
+    let num: usize = num
+        .trim()
+        .parse()
+        .expect("Please enter valid positive number");
+    let result = fib(num);
+    println!("fib of {num} is {result}");
+}
+
+fn fib(num: usize) -> usize {
+    if num <= 1 {
+        num
+    } else {
+        fib(num - 1) + fib(num - 2)
+    }
 }
